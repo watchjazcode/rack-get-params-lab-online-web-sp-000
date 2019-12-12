4c60036a557816d95a1a89bfdd8399d02a0efd91
Jazmin M <jazminmerckel@gmail.com>
2019-12-12 06:47:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,14 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+    elsif req.path.match(/cart/)
+      if @@cart == []
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item| 
+          resp.write "#{item}\n"
+        end
+      end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
