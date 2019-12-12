ffb3771e477807168ed56c3baa7cc4955f9d09d0
Jazmin M <jazminmerckel@gmail.com>
2019-12-12 06:47:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,7 @@ class Application
       if @@cart == []
         resp.write "Your cart is empty"
       else
-        @@cart.each do |item| 
+        @@cart.each do |item|
           resp.write "#{item}\n"
         end
       end
